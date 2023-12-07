eece4ebf3e1d2809652d3487c88a901bcdfcdec9
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2023-12-07 16:11:26 +0300

    fix: Resolved an issue in the PostgreSQL connection string

---
+++ HospitalWebApplication/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace HospitalWebApplication.Models
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=Hospital;Username=postgres;Password=postgres");
+        }
+
+
+
+
+
+    }
+}
