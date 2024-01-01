5f82481b7d6712d33dcecc87b4e882df386489f3
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2024-01-01 16:14:49 +0300

    Template Eklendi

--- HospitalWebApplication/Controllers/HomeController.cs
+++ HospitalWebApplication/Controllers/HomeController.cs
@@ -17,16 +17,21 @@ namespace HospitalWebApplication.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult About()
+		{
+			return View();
+		}
+		public IActionResult Services()
+		{
+			return View();
+		}
+		public IActionResult News()
+		{
+			return View();
+		}
+		public IActionResult Contact()
+		{
+			return View();
+		}
+	}
 }
